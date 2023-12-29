1765171dbe2e4df29d39ae77d351fe3334b2a734
airickLeo <88010729+airickLeo@users.noreply.github.com>
2023-12-29 16:19:51 -0500

    installed packages

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,6 @@ import Search from "./Search";
 import Sidebar from "./Sidebar";
 import { BackToTop } from "./BackToTop";
 import RecipeDetails from "./RecipeDetails";
+import Favourites from "./Favourites";
 
-export { HomePage, Search, Sidebar, BackToTop, RecipeDetails }
+export { HomePage, Search, Sidebar, BackToTop, RecipeDetails, Favourites}
